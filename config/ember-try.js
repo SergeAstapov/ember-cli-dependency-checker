d38541a52e9290819d9c279b0b5900636cde723d
Sergey Astapov <SergeAstapov@gmail.com>
2022-04-12 22:11:08 -0400

    Add ember-try scenarios for compat testing

---
+++ config/ember-try.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = async function () {
+  return {
+    command: 'yarn test',
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-cli': '~3.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-cli': '~3.16.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-cli': '~3.20.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-cli': '~3.24.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-cli': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-cli': 'npm:latest',
+          },
+        },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-cli': 'npm:beta',
+          },
+        },
+      },
+    ],
+  };
+};
